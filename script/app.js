dd4ccfe9ad545b033f3aabc527a9b418ebf2b3c3
Renato <renatomna@gmail.com>
2023-08-26 13:33:03 -0300

    feat: criação de função para obtenção dos dados de entrada.

--- script/app.js
+++ script/app.js
@@ -4,3 +4,16 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+const propriedades = [];
+
+function obterDadosEntrada(){
+    rl.question('Digite uma propriedade de CSS (ou "SAIR" para encerrar): ', entrada => {
+        if (entrada.toLocaleUpperCase() === 'SAIR') {
+            rl.close();
+        } else {
+            propriedades.push(entrada);
+            obterDadosEntrada();
+        }
+    });
+}
