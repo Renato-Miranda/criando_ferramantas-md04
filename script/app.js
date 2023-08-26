61f6abbc382aea1383a1401f7a366e54b25ea8a9
Renato <renatomna@gmail.com>
2023-08-26 13:46:23 -0300

    feat: ciração função anonima para exibição dos dados ordenados após encerramento com o comando sair.

--- script/app.js
+++ script/app.js
@@ -9,11 +9,22 @@ const propriedades = [];
 
 function obterDadosEntrada(){
     rl.question('Digite uma propriedade de CSS (ou "SAIR" para encerrar): ', entrada => {
-        if (entrada.toLocaleUpperCase() === 'SAIR') {
+        if (entrada.toUpperCase() === 'SAIR') {
             rl.close();
         } else {
-            propriedades.push(entrada);
+            propriedades.push(entrada.toUpperCase());
             obterDadosEntrada();
         }
     });
 }
+
+rl.on('close', () => {
+    const propriedadesOrdenadas = propriedades.sort();
+    console.log('\nPropriedades ordenadas de A-Z:');
+    propriedadesOrdenadas.forEach(propriedade => {
+        console.log(propriedade);
+    });
+    process.exit(0);
+});
+
+obterDadosEntrada();
